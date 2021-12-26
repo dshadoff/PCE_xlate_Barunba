67909c1a21d594dcbcb235362a9ed5687034d6c7
David Shadoff <46657586+dshadoff@users.noreply.github.com>
2021-12-26 17:39:35 -0500

    Initial Patch
    Translated, but using a font already in VRAM (which could be improved)

---
+++ extract/extract.bat
@@ -0,0 +1,28 @@
+:::: Barunba Area Titles:
+::
+::
+:: Original set of sprites for area names
+::
+..\tools\pce_gfx get sprite barunba.pce 0x02A400 1 1 1 02A400.orig 0 0 ..\xlate\02A400.xlate
+..\tools\pce_gfx get sprite barunba.pce 0x02A480 1 1 1 02A480.orig 0 0 ..\xlate\02A400.xlate
+..\tools\pce_gfx get sprite barunba.pce 0x02A500 1 1 1 02A500.orig 0 0 ..\xlate\02A400.xlate
+..\tools\pce_gfx get sprite barunba.pce 0x02A580 1 1 1 02A580.orig 0 0 ..\xlate\02A400.xlate
+..\tools\pce_gfx get sprite barunba.pce 0x02A600 1 1 1 02A600.orig 0 0 ..\xlate\02A400.xlate
+..\tools\pce_gfx get sprite barunba.pce 0x02A680 1 1 1 02A680.orig 0 0 ..\xlate\02A400.xlate
+
+..\tools\pce_gfx get sprite barunba.pce 0x02AC00 1 1 1 02AC00.orig 0 0 ..\xlate\02A400.xlate
+..\tools\pce_gfx get sprite barunba.pce 0x02AC80 1 1 1 02AC80.orig 0 0 ..\xlate\02A400.xlate
+..\tools\pce_gfx get sprite barunba.pce 0x02AD00 1 1 1 02AD00.orig 0 0 ..\xlate\02A400.xlate
+..\tools\pce_gfx get sprite barunba.pce 0x02AD80 1 1 1 02AD80.orig 0 0 ..\xlate\02A400.xlate
+..\tools\pce_gfx get sprite barunba.pce 0x02AE00 1 1 1 02AE00.orig 0 0 ..\xlate\02A400.xlate
+..\tools\pce_gfx get sprite barunba.pce 0x02AE80 1 1 1 02AE80.orig 0 0 ..\xlate\02A400.xlate
+
+..\tools\pce_gfx get sprite barunba.pce 0x02B400 1 1 1 02B400.orig 0 0 ..\xlate\02A400.xlate
+..\tools\pce_gfx get sprite barunba.pce 0x02B480 1 1 1 02B480.orig 0 0 ..\xlate\02A400.xlate
+..\tools\pce_gfx get sprite barunba.pce 0x02B500 1 1 1 02B500.orig 0 0 ..\xlate\02A400.xlate
+..\tools\pce_gfx get sprite barunba.pce 0x02B580 1 1 1 02B580.orig 0 0 ..\xlate\02A400.xlate
+
+..\tools\pce_gfx get sprite barunba.pce 0x02BC00 1 1 1 02BC00.orig 0 0 ..\xlate\02A400.xlate
+..\tools\pce_gfx get sprite barunba.pce 0x02BC80 1 1 1 02BC80.orig 0 0 ..\xlate\02A400.xlate
+..\tools\pce_gfx get sprite barunba.pce 0x02BD00 1 1 1 02BD00.orig 0 0 ..\xlate\02A400.xlate
+..\tools\pce_gfx get sprite barunba.pce 0x02BD80 1 1 1 02BD80.orig 0 0 ..\xlate\02A400.xlate
